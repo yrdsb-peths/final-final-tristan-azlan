5c112f98c4270c2fd94851e8923dd57e9c9d5127
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-12-12 13:51:03 -0500

    paddle
    gave it movement and speed

--- Paddle.java
+++ Paddle.java
@@ -15,5 +15,14 @@ public class Paddle extends Actor
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX() - 8, getY());
+        }
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX() + 8, getY());
+        }
+        
     }
 }
