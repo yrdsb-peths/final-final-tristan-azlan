b8df290c586b07ae139142e97aef0773853b06ca
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-12-12 14:03:05 -0500

    ball and world
    added ball movement and backround for world

--- Ball.java
+++ Ball.java
@@ -12,8 +12,15 @@ public class Ball extends Actor
      * Act - do whatever the Ball wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int x = 3;
+    private int y = -3;
     public void act()
     {
         // Add your action code here.
+        moveBall();
+    }
+    private void moveBall()
+    {
+        setLocation(getX() + x, getY() + y); 
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,16 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private Paddle paddle;
+    private Ball ball;
     public MyWorld() {
         super(600, 400, 1);
+        // add paddle
+        paddle = new Paddle();
+        addObject(paddle, 300, 350);
+        
+        // add ball
+        ball = new Ball();
+        addObject(ball, 300, 335);
     }
 }
