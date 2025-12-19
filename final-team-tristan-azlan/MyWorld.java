dce0cac69ef780fb406c27124906ad7aa8729414
AzlanAftabSadiq <349389189@gapps.yrdsb.ca>
2025-12-19 13:14:56 -0500

    brick pattern set

--- final-team-tristan-azlan/MyWorld.java
+++ final-team-tristan-azlan/MyWorld.java
@@ -17,5 +17,27 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 8; j++) {
+                int x = Greenfoot.getRandomNumber(12);
+                
+                if(x==1)
+                {
+                    TNT_Block block1 = new TNT_Block();
+                    addObject(block1, 55 + j * 70, 50 + i * 70);
+                }
+                else if(x==2)
+                {
+                    Extra_Ball_Block block2 = new Extra_Ball_Block();
+                    addObject(block2, 55 + j * 70, 50 + i * 70);
+                }
+                else
+                {
+                    Block block3 = new Block();
+                    addObject(block3, 55 + j * 70, 50 + i * 70);
+                }
+            }
+        }
     }
 }
